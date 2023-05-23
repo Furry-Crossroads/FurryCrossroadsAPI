19b8de46bbd7ce1d1939997571814f9fcdbaca7d
Fur Coat <tkkt392@gmail.com>
2023-05-23 09:58:59 -0400

    Added a little landing page to see.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+
+# Need to show that this environment is for a FCAPI
+if not os.environ.get("FCAPI"):
+    raise RuntimeError("FCAPI environment variable not set.")
+
 class Config(object):
     DEBUG = False
     TESTING = False
